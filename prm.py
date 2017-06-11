d23dd641164a62d95f33d8718a31f631f1358835
Dennis Fong <dfong@csil-26.cs.ucsb.edu>
2017-06-10 22:08:55 -0700

    Up until replicate()

--- prm.py
+++ prm.py
@@ -3,36 +3,39 @@
 # 8234338, 8334492
 # 22 May 2017
 
-# ./PRM siteID fileOfIPs
+# ./prmTest siteID fileOfIPs port
 
 import sys
 import socket
 import time
+import Queue
+import threading
 
-MYIP = 0
+q = Queue.Queue()
+MYIP = '127.0.0.1'
+LOCALHOST = '127.0.0.1'
 MYID = int(sys.argv[1])
-PORT = 5004
+PORT = int(sys.argv[3])
 
 # int, int
 # ballotNumber, aSiteID
-BALLOTNUM = [0, 0]
+BALLOTNUM = [0, MYID]
 ACCEPTNUM = [0, 0]
 
 # string (file name to be replicated)
-ACCEPTVAL = None
+ACCEPTVAL = "null"
 
 # string (file name proposed to be replicated)
 PROPOSEDVAL = None
 
-# dict for phase 2 to check if already have received accept
-ACCEPTDICT = {}
-
-# dict for phase 2 to check for all vals  = null
-ACKDICT = {}
-
 # string to string
 # siteID to IP
 IPDICT = {}
+
+# for testing
+# string to string
+# siteID to port
+PORTDICT = {}
 
 # the nested 3d dictionary
 THELOG = {} 
@@ -41,11 +44,11 @@ THELOG = {}
 log_number = 0
 
 # string to socket
-# IP to socket
+# ID to socket
 SOCKDICT = {}
 
-NUMACKS = 0
-NUMACCEPTS = 0
+NUMACKS = {}
+NUMACCEPTS = {}
 ISLEADER = 0
 ISRUNNING = 1
 
@@ -54,15 +57,13 @@ def setupConfig():
     with open(sys.argv[2], 'r') as configFile:
         for line in configFile:
             line = line.split()
-            if MYID in line[0]:
-                MYIP = line[0]
-            else:
-                IPDICT[line[0]] = line[1]
+            if str(MYID) not in line[0]:
+                PORTDICT[line[0]] = line[1]
 
 # connect to all other PRMs
 def setupPorts():
-    for siteID in IPDICT:
-        addr = (IPDICT[siteID], 5004)
+    for siteID in PORTDICT:
+        addr = (LOCALHOST, int(PORTDICT[siteID]))
         SOCKDICT[siteID] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         SOCKDICT[siteID].connect(addr)
 
@@ -70,7 +71,8 @@ def setupPorts():
 def checkStream():
     global BALLOTNUM, PROPOSEDVAL, NUMACKS, NUMACCEPTS, ISLEADER, ISRUNNING, ACCEPTNUM, ACCEPTVAL
     try:
-        rawData = servsock.recv(1024)
+        rawData = q.get()
+        print rawData
         splitData = rawData.split()
         for ballot in splitData:
             ballotArgs = ballot.split(',')
@@ -84,6 +86,8 @@ def checkStream():
                     BALLOTNUM[0] = BALLOTNUM[0] + 1
                     PROPOSEDVAL = ballotArgs[1]
                     ISLEADER = 1
+                    balKey = str(BALLOTNUM[0]) + "." + str(BALLOTNUM[1])
+                    NUMACKS[balKey] = 0
                     sendPrepare()
                 if "prepare" in ballot:
                     # prepare,ballot.num, ballot.ID
@@ -91,29 +95,32 @@ def checkStream():
                     incomingID = int(ballotArgs[2])
                     incomingBallot = [incomingNum, incomingID]
                     if firstGreater(incomingBallot, BALLOTNUM):
-                        BALLOTNUM[0] = prepareNum
-                        BALLOTNUM[1] = prepareID
+                        BALLOTNUM[0] = incomingNum
+                        BALLOTNUM[1] = incomingID
                         sendAck(incomingBallot)
                 if "ack" in ballot:
                     # ack,proposedBal.num,proposedBal.ID,acceptBal.num,acceptBal.ID,acceptVal
-                    NUMACKS = NUMACKS + 1
-                    if NUMACKS == 1:
+                    print "Got ack"
+                    ackKey = ballotArgs[1] + "." + ballotArgs[2]
+                    NUMACKS[ackKey] += 1
+                    if NUMACKS[ackKey] == 1:
                         incomingBal  = [int(ballotArgs[1]), int(ballotArgs[2])]
                         incomingAcceptBal = [int(ballotArgs[3]), int(ballotArgs[4])]
                         incomingVal = ballotArgs[5]
-                        if (incomingVal or ACCEPTVAL) is None:
+                        if (incomingVal and ACCEPTVAL) is "null":
                             myTempVal = PROPOSEDVAL
                         else:
                             #does this work? only 3 nodes so maybe?
                             if firstGreater(ACCEPTNUM, incomingAcceptBal):
-                                myTempVal = PROPOSEDVAL
+                                myTempVal = ACCEPTVAL
                             else:
                                 myTempVal = incomingVal
-                        leaderAccept(myTempVal)
+                        leaderAccept(myTempVal, ackKey)
                 if "accept" in ballot:
                     # accept,ballotNum.num,ballotNum.ID,myTempVal
-                    NUMACCEPTS = NUMACCEPTS + 1
-                    if NUMACCEPTS = 1:
+                    acceptKey = ballotArgs[1] + "." + ballotArgs[2]
+                    if acceptKey not in NUMACCEPTS:
+                        NUMACCEPTS[acceptKey] = 1 
                         incomingBal = [int(ballotArgs[1]), int(ballotArgs[2])]
                         incomingAcceptVal = ballotArgs[3]
                         if firstGreater(incomingBal, BALLOTNUM):
@@ -121,9 +128,9 @@ def checkStream():
                             ACCEPTNUM[1] = incomingBal[1]
                             ACCEPTVAL = incomingAcceptVal
                             cohortAccept(incomingBal, incomingAcceptVal)
-                        if (ISLEADER):
-                            decide()
-                            'decide,makenewlog,words'
+                    elif NUMACCEPTS[acceptKey] == 1:
+                        NUMACCEPTS[acceptKey] += 1
+                        decide()
                 if "decide" in ballot:
                     # decide,fileNameToReplicate
                     stringToLog(ballotArgs[1])
@@ -134,36 +141,47 @@ def checkStream():
                     print_log()
                 if "merge" in ballot:
                     merge(ballotArgs[1], ballotArgs[2])
-                    
+    except Exception,e:
+        print str(e)
             
 def firstGreater(ballot1, ballot2):
     if ballot1[0] > ballot2[0]:
-        return true
+        return True
     elif ballot1[0] == ballot2[0]:
         if ballot1[1] > ballot2[1]:
-            return true
+            return True
+        elif ballot1[1] == ballot2[1]:
+            return True
         else:
-            return false
+            return False
     else:
-        return false
+        return False
 
 def sendPrepare():
     for sock in SOCKDICT:
         SOCKDICT[sock].sendall("prepare," + str(BALLOTNUM[0]) + "," + str(BALLOTNUM[1]) + " ")
+    print "Sent prepare"
 
 def sendAck(ballot):
+    # ballotNum, ballotID
+    # int, int
     destination = str(ballot[1])
-    SOCKDICT[destination].sendall("ack," + str(ballot[0]) + "," + str(ballot[1]) + "," + str(ACCEPTNUM[0]) + "," + str(ACCEPTNUM[1]) + "," + ACCEPTVAL)
+    message = "ack," + str(ballot[0]) + "," + str(ballot[1]) + "," + str(ACCEPTNUM[0]) + "," + str(ACCEPTNUM[1]) + "," + ACCEPTVAL
+    
+    SOCKDICT[destination].sendall(message)
 
-def leaderAccept(tempAcceptVal):
+def leaderAccept(tempAcceptVal, ackKey):
     for sock in SOCKDICT:
-        SOCKDICT[sock].sendall("accept," + str(BALLOTNUM[0]) + "," + tempAcceptVal)
+        SOCKDICT[sock].sendall("accept," + str(BALLOTNUM[0]) + "," + str(BALLOTNUM[1]) + "," + tempAcceptVal)
+    NUMACCEPTS[ackKey] = 1
+    
                                
 def cohortAccept(b, v):
     for sock in SOCKDICT:
         SOCKDICT[sock].sendall("accept," + str(b[0]) + "," + str(b[1]) + "," + v)
 
 def decide():
+    print "Received majority, decide()"
     replicate(ACCEPTVAL)
                                
 # PRM FUNCTIONS #
@@ -206,11 +224,13 @@ def print_log():
 
 def replicate(filename):
 ## placeholders for code referencing ##
+    global log_number
+    print "Inside replicate"
     THELOG[log_number] = {}       ##log_number = whichever log the file is stored in order
     THELOG[log_number]['words'] = {}
     THELOG[log_number]['name'] = filename
-    
 ## end of placeholders ##
+
     readfile = open(filename, 'r')
     for line in readfile:
         if (len(line.split()) != 2):
@@ -225,17 +245,18 @@ def replicate(filename):
     words = THELOG[log_number]['words']        
     rep_log = THELOG[log_number]['name'] + '/'
     for word in words:
-        rep_log = rep_log + word + '+' words[word] + '/' 
+        rep_log = rep_log + word + '+' + words[word] + '/' 
     for sock in SOCKDICT:
         SOCKDICT[sock].sendall("decide," + rep_log)    
     #send rep_log to other PRMs to replicate
 
     log_number = log_number + 1
+    print "End of replicate"
     
 def stringToLog(logString):
     words = logString.split('/')
     updatedName = False          #check if log has updated name
-    if(log_number not in THELOG)
+    if log_number not in THELOG:
         THELOG[log_number] = {}
     THELOG[log_number]['words'] = {}
     for line in words:
@@ -247,18 +268,66 @@ def stringToLog(logString):
             wc = int(line.split('+')[1])
             THELOG[log_number]['words'][word] = wc                      
 
+def thread1():
+    stream1, addr1 = servsock.accept()
+    while True:
+        try:
+            data = stream1.recv(1024)
+            q.put(data)
+        except KeyboardInterrupt:
+            servsock.close()
+
+def thread2():
+    stream2, addr2 = servsock.accept()
+    while True:
+        try: 
+            data = stream2.recv(1024)            
+            q.put(data)
+        except KeyboardInterrupt:
+            servsock.close()
+
+def thread3():
+    stream3, addr3 = servsock.accept()
+    while True:
+        try:
+            data = stream3.recv(1024)
+            q.put(data)
+        except KeyboardInterrupt:
+            servsock.close()
+
 # the main function
 setupConfig()
+print "Config set up"
+
 servsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 servsock.bind((LOCALHOST, PORT))
 servsock.listen(10)
+time.sleep(3)
 
-time.sleep(5)
 setupPorts()
 
-# keep while loop running to checkStream()
+t1 = threading.Thread(target = thread1)
+t2 = threading.Thread(target = thread2)
+t3 = threading.Thread(target = thread3)
 
-servsock.close()
-for sock in SOCKDICT:
-    SOCKDICT[sock].close()
+t1.daemon = True
+t2.daemon = True
+t3.daemon = True
+
+t1.start()
+t2.start()
+t3.start()
+
+print "Ports setup"
+
+# keep while loop running to checkStream()
+try:
+    while True:
+        checkStream()
+except KeyboardInterrupt:
+    sys.exit()
+finally:
+    servsock.close()
+    for sock in SOCKDICT:
+        SOCKDICT[sock].close()
